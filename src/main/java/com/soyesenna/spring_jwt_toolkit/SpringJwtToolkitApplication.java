e9346a416a71a4a802806d308ca807a73b48af09
soyesenna <kjy915875@gmail.com>
2025-11-13 23:32:51 +0900

    initial commit

---
+++ src/main/java/com/soyesenna/spring_jwt_toolkit/SpringJwtToolkitApplication.java
@@ -0,0 +1,13 @@
+package com.soyesenna.spring_jwt_toolkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtToolkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtToolkitApplication.class, args);
+	}
+
+}
